e74203e203356939694903b1ca5386ea2cd0589d
Jamie Davis <jamied1985@gmail.com>
2018-11-12 12:02:38 +0000

    second commit

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProjctList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-1 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Jamie Davis</p>
+          <p className="grey-text">12 November</p>
+        </div>
+      </div>
+      <div className="card z-depth-1 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Jamie Davis</p>
+          <p className="grey-text">12 November</p>
+        </div>
+      </div>
+      <div className="card z-depth-1 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Jamie Davis</p>
+          <p className="grey-text">12 November</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjctList;
