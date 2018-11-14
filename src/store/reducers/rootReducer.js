6dc9470068869674dfafe0ef2f0dbba3a97140e9
Jamie Davis <jamied1985@gmail.com>
2018-11-14 14:04:09 +0000

    connected dashboard to firestore

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from "./authoReducer";
 import projectReducers from "./projectReducer";
 import { combineReducers } from "redux";
+import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  project: projectReducers
+  project: projectReducers,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
